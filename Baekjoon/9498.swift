ea7cbed3da935f933c7986f1c38d699e98a1fcf2
beansBin <beans_bin@naver.com>
2021-12-27 03:00:56 +0900

    Add Baekjoon 9498

---
+++ Baekjoon/9498.swift
@@ -0,0 +1,13 @@
+var input = Int(readLine()!)!
+
+if input >= 90 {
+  print("A")
+} else if input >= 80 {
+  print("B")
+} else if input >= 70 {
+  print("C")
+} else if input >= 60 {
+  print("D")
+} else {
+  print("F")
+}
